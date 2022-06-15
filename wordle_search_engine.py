f35a79fefe2d8ca02429d6cc62a1b1b48312b18f
Michael Mailloux <mmailloux22@gmail.com>
2022-06-15 18:50:31 -0400

    Added excluded letters functionality and fixed many bugs

--- wordle_search_engine.py
+++ wordle_search_engine.py
@@ -8,8 +8,12 @@ def read_data(filename):
     return wordlelist
 
 def wordle_search(wordlelist) -> None:
+    
+    excluded_set = set()
+    excluded_letters_string = ''
+    
     while True:
-        
+
         matches = []
 
         while len(matches) == 0:
@@ -22,12 +26,21 @@ def wordle_search(wordlelist) -> None:
             if len(search_term) > 5:
                 print('Your search term MUST be 5 characters or less!')
 
-            else:    
-                excluded_letters = input('Enter new excluded letters: ')
+            else:
+                excluded_letters = input('Enter excluded letters: ')
 
-                if excluded_letters.isalpha():
-                    search_term = search_term + '*' +'[^' + excluded_letters + ']'
+                if excluded_letters:
+                    for letter in excluded_letters:
+                        excluded_set.add(letter)
                     
+                    excluded_letters_string = ''
+                    for letter in excluded_set:
+                        if letter.isalpha():
+                            excluded_letters_string += letter + '|'
+
+                if excluded_letters_string:
+                    search_term = '^(?!.*(' + excluded_letters_string[:-1] + ')).*' + search_term
+                
                 matches = [i for i in wordlelist if re.match(search_term, i, flags=re.IGNORECASE)]
                 if len(matches) == 0 and len(search_term.split('*')[0]) <= 5:
                     print('There are no matches for your search term.')
@@ -35,6 +48,16 @@ def wordle_search(wordlelist) -> None:
         print('\nResults:')
         for match in matches:
             print(match)
+        
+        print('\nContinue searching for the same Wordle?')
+        continue_search = input("If yes, hit Enter. To search for a new wordle enter 'w'. To exit enter q:\n")
+
+        if continue_search == 'w':
+            excluded_set.clear()
+            excluded_letters_string = ''
+
+        if continue_search == 'q':
+            exit()
 
 if __name__ == '__main__':
     print('\nWordle Search Engine\n')
